9e68c0f94779bd6e08ff3b1da9753bb8c057ecd4
Josh Dallos <joshdallos@gmail.com>
2022-03-04 16:09:40 +0200

    requested changes made

---
+++ src/features/generic/time-utils.ts
@@ -0,0 +1,21 @@
+import { Duration } from "luxon";
+
+/**
+ *  namespace for time utilities
+ */
+namespace TimeUtilities {
+
+    /**
+   * Converts minutes into hours and minutes
+   *
+   * @param duration in minutes from timebank Data
+   * @returns string of timebank data converted to hours and minutes
+   */
+    export const timeConversion = (duration: number): string => {
+      const dur = Duration.fromObject({ minutes: duration });
+      const time = dur.shiftTo("hours", "minutes");
+      return `${time.hours}h ${time.minutes} minutes`;
+    };
+};
+
+export default TimeUtilities;
--- src/features/slackapi/slackapi-utils.ts
+++ src/features/slackapi/slackapi-utils.ts
@@ -2,7 +2,8 @@ import { FormattedTimebankData } from "@functions/sendslack/schema";
 import { LogLevel, WebClient } from "@slack/web-api";
 import { Member } from "@slack/web-api/dist/response/UsersListResponse";
 import dotenv from "dotenv";
-import { DateTime, Duration } from "luxon";
+import { DateTime } from "luxon";
+import TimeUtilities from "../generic/time-utils";
 
 dotenv.config({ path: __dirname + "/../../../../../.env" });
 
@@ -31,18 +32,6 @@ namespace SlackApiUtilities {
   };
 
   /**
-   * Converts minutes into hours and minutes
-   *
-   * @param duration in minutes from timebank Data
-   * @returns string of timebank data converted to hours and minutes
-   */
-  const timeConversion = (duration: number): string => {
-    const dur = Duration.fromObject({ minutes: duration });
-    const time = dur.shiftTo("hours", "minutes");
-    return `${time.hours}h ${time.minutes} minutes`;
-  };
-
-  /**
    * Create message based on specific users timebank data
    *
    * @param filteredTimebankData list of relevant timebank data
@@ -52,7 +41,7 @@ namespace SlackApiUtilities {
     const { name, logged, expected, date } = filteredTimebankData;
     const displayDate = DateTime.fromISO(date).toFormat("dd-MM-yyyy");
 
-    return `Hi ${name}, yesterday (${displayDate}) you worked ${timeConversion(logged)} with an expected time of ${timeConversion(expected)}. Have a great rest of the day!`;
+    return `Hi ${name}, yesterday (${displayDate}) you worked ${TimeUtilities.timeConversion(logged)} with an expected time of ${TimeUtilities.timeConversion(expected)}. Have a great rest of the day!`;
   };
 
 

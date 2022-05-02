8ec2831e7aa9598bc15da9e6b0bb9f885ee3a167
Josh Dallos <joshdallos@gmail.com>
2022-05-02 16:26:00 +0300

    .coveralls

--- jest.config.ts
+++ jest.config.ts
@@ -4,7 +4,6 @@ module.exports = {
   moduleNameMapper: {
     "src/(.*)": "<rootDir>/src/$1",
     "tests/(.*)": "<rootDir>/__tests__/$1",
-    
   },
   coveragePathIgnorePatterns: [
     "src/generated/*",

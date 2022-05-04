d310b38a46008e2493b351d418db7e0cf364042a
jonihamalainen <jomppe21@gmail.com>
2022-05-04 12:10:51 +0300

    Update src/features/timebank/timebank-API-provider.ts
    Co-authored-by: Jari Nykänen <45241047+jarinykanen@users.noreply.github.com>

--- src/features/timebank/timebank-API-provider.ts
+++ src/features/timebank/timebank-API-provider.ts
@@ -15,7 +15,7 @@ namespace TimeBankApiProvider {
   export const getTimebankUsers = async (): Promise<PersonDto[]> => {
     try {
       const { body } = await client.timebankControllerGetPersons();
-      if(body.length === 0){
+      if (!body.length) {
         throw new Error("Error while loading persons from Timebank");
       }
       return body.filter(person => person.defaultRole !== null);
